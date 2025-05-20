2d72dd33727b1ddb72c0f6609ac083a9add5d680
sebastian <fuze1276@gmail.com>
2025-05-20 08:19:03 -0500

    Actualización de conexion.py con datos de conexión remota

--- conexion.py
+++ conexion.py
@@ -1,16 +1,20 @@
 import psycopg2
+import os
 
-# Conexión a la base de datos PostgreSQL
+import psycopg2
+
+# Conexión a base de datos remota (Clever Cloud)
 try:
     conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",      # Cambia si tu base se llama diferente
-        user="postgres",          # Tu usuario de PostgreSQL
-        password="THEGHOST7274"   # Tu contraseña de PostgreSQL
+        host='bvwfyr03z7nd6awox7a5-postgresql.services.clever-cloud.com',
+        user='us0nxqdxueurk6pgmiyr',
+        password='n1vF2qL2ZZI95yT9ipcv3atZuwOYwy',
+        database='bvwfyr03z7nd6awox7a5',
+        port='50013'
     )
     cursor = conn.cursor()
-    print("✅ Conexión a PostgreSQL establecida correctamente.")
+    print("✅ Conexión a PostgreSQL (remota) establecida correctamente.")
 except Exception as e:
-    print("❌ Error al conectar con la base de datos:", e)
+    print("❌ Error al conectar con la base de datos:", type(e).__name__, e)
     conn = None
     cursor = None
